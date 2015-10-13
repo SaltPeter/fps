decfac4ea93445a853fbf3670a5cc982970c6c93
Peter Tran <peter.tran3@carleton.ca>
2015-10-13 12:22:07 -0400

    Update bl_BodyPart.cs

--- Player/bl_BodyPart.cs
+++ Player/bl_BodyPart.cs
@@ -1,14 +1,12 @@
-// bl_BodyPart.cs
-// This script receives the information of the damage done by another player
+// bl_BodyPart.cs - This script receives the information of the damage done by another player
 // place it on a gameobject containing a collider in the hierarchy of the remote player
 // use "bl_BodyPartManager.cs" to automatically configure                       
 using UnityEngine;
 
 public class bl_BodyPart : MonoBehaviour {
-    public float multiplier = 1f; /// so far as the damage is multiplied in this part
+    public float multiplier = 1f; // Damage multiplier, multiplies damage taken
     public bl_PlayerDamageManager HealtScript;
-    /// Its this the heat? if yes them is heatshot :)
-    public bool TakeHeatShot = false;
+    public bool TakeHeatShot = false; // Is it the head?
 
     /// Use this for recive damage local and sync for all other
     public void GetDamage(float t_damage, string t_from, string t_weapon,Vector3 t_direction,int weapon_ID = 0)
